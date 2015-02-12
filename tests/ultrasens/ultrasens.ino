3d6eb29e1ed8b4affdc5c7dc9ab7acdb7422a597
alexander ershov <alexander.ershov@gmailcom>
2015-02-12 20:34:27 +0300

    TTT

--- tests/ultrasens/ultrasens.ino
+++ tests/ultrasens/ultrasens.ino
@@ -4,54 +4,61 @@ echo DLR=100,100 > /dev/ttyATH0
 cat /dev/ttyATH0
 */
 
+//#define TTY_SPEED 38400
 #define TTY_SPEED 38400
 
 #define US_IN    P1_6
 #define US_OUT   P1_7
 
+#define inputPin    P1_6
+#define outputPin   P1_7
+
 volatile int16_t us_dist=0; 
       
 void setup()
 {
-  analogFrequency(100);
+  //analogFrequency(100);
 
   pinMode(US_OUT, OUTPUT);     
   pinMode(US_IN, INPUT);       
-  pinMode(RED_LED, OUTPUT);     
+  //pinMode(RED_LED, OUTPUT);     
  
    Serial.begin(TTY_SPEED);
-   digitalWrite(RED_LED, HIGH);  
-   delay(100);
-   digitalWrite(RED_LED, LOW);
+   //digitalWrite(RED_LED, HIGH);  
+   //delay(100);
+   //digitalWrite(RED_LED, LOW);
 
-    while (Serial.available()) Serial.read();  // eat garbage
+    //while (Serial.available()) Serial.read();  // eat garbage
 
 }
 
 void loop()
 {
   // put your main code here, to run repeatedly:
-  /*
+  
   digitalWrite(outputPin, LOW);  
   delayMicroseconds(2); 
   digitalWrite(outputPin, HIGH); // Pulse for 10 s to trigger ultrasonic  detection 
   delayMicroseconds(10); 
   digitalWrite(outputPin, LOW);    
-  uint32_t distance = pulseIn(inputPin, HIGH, 25000);  // Read receiver pulse time. TO is 25ms. 
+  //uint32_t distance = pulseIn(inputPin, HIGH, 25000);  // Read receiver pulse time. TO is 25ms.
+ uint32_t distance = pulseIn(inputPin, HIGH); 
   int16_t d=(int16_t)(distance/58);
   Serial.println(d);   //Ourput distance                  
-  */
+  
   
   delay(1000);   
   
+  /*
   readUSDist(); 
   
-   Notify(); 
+   Notify();
+  */ 
 }
 
 void Notify() {
-  addJson("U", (int16_t)(us_dist));
-  Serial.println();
+  //addJson("U", (int16_t)(us_dist));
+  Serial.println(us_dist);
 }
 
 void readUSDist() {
